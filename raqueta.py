7fe9471014f290c7523549c8542770b9f10444f8
Angel <angelmp200@gmail.com>
2022-12-01 18:38:24 +0100

    Update raqueta.py

--- raqueta.py
+++ raqueta.py
@@ -4,6 +4,9 @@ from pygame.locals import QUIT
 from bola import PelotaPong
 from bola import Margen_H
 from bola import Margen_V
+from bola import FPS
+BLANCO = (255, 255, 255) 
+NEGRO = (0, 0, 0)
 class RaquetaPong:
     def __init__(self):
         self.imagen = pygame.image.load("raqueta.png").convert_alpha()
@@ -27,7 +30,8 @@ class RaquetaPong:
 
         self.y += self.dir_y
     def golpear(self, pelota):
-            if (
+
+        if (
             pelota.x < self.x + self.ancho
             and pelota.x > self.x
             and pelota.y + pelota.alto > self.y
@@ -37,7 +41,7 @@ class RaquetaPong:
              pelota.x = self.x + self.ancho
 
     def golpear_ia(self, pelota):
-            if (
+        if (
             pelota.x + pelota.ancho > self.x
             and pelota.x < self.x + self.ancho
             and pelota.y + pelota.alto > self.y
@@ -45,3 +49,68 @@ class RaquetaPong:
         ):
              pelota.dir_x = -pelota.dir_x
              pelota.x = self.x - pelota.ancho    
+def main():
+    # Inicialización de Pygame
+    pygame.init()
+
+    # Inicialización de la superficie de dibujo (display surface)
+    ventana = pygame.display.set_mode((Margen_H, Margen_V))
+    pygame.display.set_caption("Pong 9")
+
+    # Inicialización de la fuente
+    fuente = pygame.font.Font(None, 60)
+
+    pelota = PelotaPong("bola_roja.png")
+
+    raqueta_1 = RaquetaPong()
+    raqueta_1.x = 60
+
+    raqueta_2 = RaquetaPong()
+    raqueta_2.x = Margen_H - 60 - raqueta_2.ancho
+
+    # Bucle principal
+    jugando = True
+    while jugando:
+        pelota.mover()
+        pelota.rebotar()
+        raqueta_1.mover()
+        raqueta_2.mover_ia(pelota)
+        raqueta_1.golpear(pelota)
+        raqueta_2.golpear_ia(pelota)
+
+        ventana.fill(BLANCO)
+        ventana.blit(pelota.imagen, (pelota.x, pelota.y))
+        ventana.blit(raqueta_1.imagen, (raqueta_1.x, raqueta_1.y))
+        ventana.blit(raqueta_2.imagen, (raqueta_2.x, raqueta_2.y))
+
+        texto = f"{pelota.puntuacion} : {pelota.puntuacion_ia}"
+        letrero = fuente.render(texto, False, NEGRO)
+        ventana.blit(letrero, (Margen_H / 2 -
+        fuente.size(texto)[0] / 2, 50))
+
+        for event in pygame.event.get():
+            if event.type == QUIT:
+                jugando = False
+
+            # Detecta que se ha pulsado una tecla
+            if event.type == pygame.KEYDOWN:
+                if event.key == pygame.K_w:
+                    raqueta_1.dir_y = -5
+                if event.key == pygame.K_s:
+                    raqueta_1.dir_y = 5
+
+            # Detecta que se ha soltado la tecla
+            if event.type == pygame.KEYUP:
+                if event.key == pygame.K_w:
+                    raqueta_1.dir_y = 0
+                if event.key == pygame.K_s:
+                    raqueta_1.dir_y = 0
+
+        pygame.display.flip()
+        pygame.time.Clock().tick(FPS)
+
+    pygame.quit()
+
+
+if __name__ == "__main__":
+    main()
