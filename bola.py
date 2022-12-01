24c7bd6cc8c4b29bc0efb404601dedf92d82931b
mgonzalz <mgonzgarc@myuax.com>
2022-12-01 18:23:57 +0100

    Paso 3-4, junto a los anteriores

--- bola.py
+++ bola.py
@@ -3,10 +3,11 @@ import pygame
 from pygame.locals import QUIT
 Margen_V = 600
 Margen_H = 800
-Fondo_Blanco = (225, 225, 225)
+FPS = 60
+Fondo_Blanco = (255, 255, 255)
 class PelotaPong:
     def __init__(self, fichero_imagen):
-# Imagen de la Pelota
+        # Imagen de la Pelota
         self.imagen = pygame.image.load(fichero_imagen).convert_alpha()
 
         # Dimensiones de la Pelota
@@ -19,18 +20,33 @@ class PelotaPong:
         # Dirección de movimiento de la Pelota
         self.dir_x = random.choice([-5, 5])
         self.dir_y = random.choice([-5, 5])
-def mover(self):
+
+    def mover(self):
         self.x += self.dir_x
         self.y += self.dir_y
 
+    def rebotar(self):
+        if self.x <= 0:
+            self.dir_x = -self.dir_x
+        if self.x + self.ancho >= Margen_H:
+            self.dir_x = -self.dir_x
+        if self.y <= 0:
+            self.dir_y = -self.dir_y
+        if self.y + self.alto >= Margen_V:
+            self.dir_y = -self.dir_y
+    def reiniciar(self):
+        self.x = Margen_H / 2 - self.ancho / 2
+        self.y = Margen_V / 2 - self.alto / 2
+        self.dir_x = -self.dir_x
+        self.dir_y = random.choice([-5, 5])
 
 def main():
     # Inicialización de Pygame
     pygame.init()
 
     # Inicialización de la superficie de dibujo (display surface)
-    ventana = pygame.display.set_mode((VENTANA_HORI, VENTANA_VERT))
-    pygame.display.set_caption("Pong 2")
+    ventana = pygame.display.set_mode((Margen_H, Margen_V))
+    pygame.display.set_caption("Pong")
 
     pelota = PelotaPong("bola_roja.png")
 
@@ -38,8 +54,9 @@ def main():
     jugando = True
     while jugando:
         pelota.mover()
+        pelota.rebotar()
 
-        ventana.fill()
+        ventana.fill(Fondo_Blanco)
         ventana.blit(pelota.imagen, (pelota.x, pelota.y))
 
         for event in pygame.event.get():
@@ -50,7 +67,6 @@ def main():
         pygame.time.Clock().tick(FPS)
 
     pygame.quit()
-
 
 if __name__ == "__main__":
     main()
