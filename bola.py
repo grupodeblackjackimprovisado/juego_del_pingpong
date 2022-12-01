bd8c82c04533ed1f0ad6171df70992d2f48dcec7
ferniprogrammer <fsanchor@myuax.com>
2022-12-01 17:40:48 +0100

    Parte 1 de la pelota hecha

--- bola.py
+++ bola.py
@@ -3,6 +3,7 @@ import pygame
 from pygame.locals import QUIT
 Margen_V = 600
 Margen_H = 800
+Fondo_Blanco = (225, 225, 225)
 class PelotaPong:
     def __init__(self, fichero_imagen):
 # Imagen de la Pelota
@@ -18,3 +19,38 @@ class PelotaPong:
         # Dirección de movimiento de la Pelota
         self.dir_x = random.choice([-5, 5])
         self.dir_y = random.choice([-5, 5])
+def mover(self):
+        self.x += self.dir_x
+        self.y += self.dir_y
+
+
+def main():
+    # Inicialización de Pygame
+    pygame.init()
+
+    # Inicialización de la superficie de dibujo (display surface)
+    ventana = pygame.display.set_mode((VENTANA_HORI, VENTANA_VERT))
+    pygame.display.set_caption("Pong 2")
+
+    pelota = PelotaPong("bola_roja.png")
+
+    # Bucle principal
+    jugando = True
+    while jugando:
+        pelota.mover()
+
+        ventana.fill()
+        ventana.blit(pelota.imagen, (pelota.x, pelota.y))
+
+        for event in pygame.event.get():
+            if event.type == QUIT:
+                jugando = False
+
+        pygame.display.flip()
+        pygame.time.Clock().tick(FPS)
+
+    pygame.quit()
+
+
+if __name__ == "__main__":
+    main()
